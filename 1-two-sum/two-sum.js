c7f8642bb6b66846e0f3a6240256f8374add4a09
Erfan <147838919+EmirErfan@users.noreply.github.com>
2026-06-07 11:24:16 +0800

    Time: 32 ms (34.64%) | Memory: 54.3 MB (79.04%) - LeetSync

--- 1-two-sum/two-sum.js
+++ 1-two-sum/two-sum.js
@@ -4,14 +4,12 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
-    
-    for (let i=0; i < nums.length; i++){
-        for (let j=i+1; j < nums.length; j++){
-            if(target === nums [j] + nums [i])
-            return [i, j];
+    for (let i = 0; i < nums.length; i ++){
+        for (let j = i+1; j <nums.length; j++){
+            if (target === nums [j] + nums [i]){
+                return [i,j];
+            }
         }
     }
-
     return [];
-
 };
